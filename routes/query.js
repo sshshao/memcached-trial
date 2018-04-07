4f23b65d8130e646642bee42879df4bed9d67daf
S Shao <sshao14@gmail.com>
2018-04-07 17:43:28 -0400

    accepting multiple statements

--- routes/query.js
+++ routes/query.js
@@ -14,28 +14,28 @@ exports.get = function(req, res) {
                 'WHERE `Club`=?' +
                 'AND `POS`=?' +
             ');' +
-        'SELECT AVG(`A`) FROM `assists` WHERE `Club`=? AND `POS`=?';
-    
+        'SELECT AVG(`A`) as `A` FROM `assists` WHERE `Club`=? AND `POS`=?';
+
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: pass.MYSQL_PW,
-        database: pass.MYSQL_DB
+        database: pass.MYSQL_DB,
+        multipleStatements: true
     });
-    
+
     connection.connect();
     connection.query(q, [club, pos, club, pos, club, pos], function(err, results, fields) {
-        console.log(results);
-        /*
+        console.log(results[0][0]);
+        console.log(results[0][1]);
+        
         res.send({
             'club': club,
             'pos': pos,
-            'max_assists': results[0].A,
-            'player': result[0].Player,
-
+            'max_assists': results[0][0].A,
+            'player': result[0][0].Player,
+            'avg_assists': result[1][0].A
         });
-        */
-       res.send({'status': 'OK'});
     });
     connection.end();
 }
